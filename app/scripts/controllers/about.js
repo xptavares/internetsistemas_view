9b008a97ccdc218a7763a56b9af324a2dfd4f771
Alexandre Tavares <xptavares@gmail.com>
2016-02-03 21:57:57 -0200

    create project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name internetsistemasViewApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the internetsistemasViewApp
+ */
+angular.module('internetsistemasViewApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name internetsistemasViewApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the internetsistemasViewApp
+ */
+angular.module('internetsistemasViewApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
