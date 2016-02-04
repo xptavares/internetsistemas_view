61ecbd534cd0277011b2126a9935a671b83398b4
Alexandre Tavares <xptavares@gmail.com>
2016-02-04 11:14:36 -0200

    fix navs and inputs numbers

---
+++ app/scripts/controllers/navigation.js
@@ -0,0 +1,6 @@
+angular.module('internetsistemasViewApp')
+  .controller('NavigationCtrl', ['$scope', '$location', function ($scope, $location) {
+    $scope.isCurrentPath = function (path) {
+      return $location.path() == path;
+    };
+  }]);
